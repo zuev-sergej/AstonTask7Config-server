3b42c816dcf3077d6c060da237b35e0922d1ccfd
Zuev_Sergey <zuevkajo@gmail.com>
2026-09-18 20:42:42 +0300

    Initial commit

---
+++ src/test/java/ru/aston/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aston.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
